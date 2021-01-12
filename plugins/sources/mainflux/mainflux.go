08505aa797bb243a540b7d2bef6e9d4c3556a4a2
Darko Draskovic <darko.draskovic@gmail.com>
2021-01-12 17:46:52 +0100

    Add debug messages
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

--- plugins/sources/mainflux/mainflux.go
+++ plugins/sources/mainflux/mainflux.go
@@ -23,9 +23,10 @@ type mainfluxSourceConfig struct {
 }
 
 type mainfluxSource struct {
-	pubSub   nats.PubSub
 	consumer chan<- api.SourceTuple
 	errCh    chan<- error
+	logger   api.Logger
+	pubSub   nats.PubSub
 	topic    string
 }
 
@@ -34,13 +35,13 @@ var _ api.Source = (*mainfluxSource)(nil)
 func (s *mainfluxSource) Configure(topic string, props map[string]interface{}) error {
 	cfg := &mainfluxSourceConfig{}
 	if err := common.MapToStruct(props, cfg); err != nil {
-		return fmt.Errorf("read properties %v fail with error: %v", props, err)
+		return fmt.Errorf("Read properties %v fail with error: %v", props, err)
 	}
 	if cfg.Host == "" {
-		return fmt.Errorf("property host is required")
+		return fmt.Errorf("Property Host is required.")
 	}
 	if cfg.Port == "" {
-		return fmt.Errorf("property port is required")
+		return fmt.Errorf("Property Port is required.")
 	}
 
 	addr := fmt.Sprintf("tcp://%s:%s/", cfg.Host, cfg.Port)
@@ -64,14 +65,16 @@ func (s *mainfluxSource) Configure(topic string, props map[string]interface{}) e
 
 func (s *mainfluxSource) Open(ctx api.StreamContext, consumer chan<- api.SourceTuple, errCh chan<- error) {
 	logger := ctx.GetLogger()
-	logger.Debug("open mainflux source")
+	logger.Debug("Open mainflux source.")
 
 	err := s.pubSub.Subscribe(s.topic, s.handle)
 	if err != nil {
 		errCh <- fmt.Errorf("Failed to subscribe to nats topic %s with error: %v", s.topic, err)
 		return
 	}
+	logger.Debugf("Subscribed to nats topic %s.", s.topic)
 
+	s.logger = logger
 	s.consumer = consumer
 	s.errCh = errCh
 
@@ -79,6 +82,8 @@ func (s *mainfluxSource) Open(ctx api.StreamContext, consumer chan<- api.SourceT
 }
 
 func (s *mainfluxSource) handle(message messaging.Message) error {
+	s.logger.Debugf("Received SenML message %v.", message)
+
 	meta := make(map[string]interface{})
 	meta["channel"] = message.Channel
 	meta["subtopic"] = message.Subtopic
@@ -109,7 +114,10 @@ func (s *mainfluxSource) handle(message messaging.Message) error {
 }
 
 func (s *mainfluxSource) Close(ctx api.StreamContext) error {
-	s.pubSub.Close()
+	if s.pubSub != nil {
+		s.pubSub.Close()
+	}
+
 	return nil
 }
 
